a71bb2125d31246b814ee1dd43f8433b65dc2413
Treyvon Pearson <tpearson3@my.waketech.edu>
2026-03-06 13:11:53 -0500

    first

--- script.js
+++ script.js
@@ -2,25 +2,56 @@
 async function fetchData() {
   try {
     const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
-    const response   = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
     if (!response.ok) {
       throw new Error("Could not fetch resource");
     }
 
-    const data          = await response.json();
+    const data = await response.json();
     const pokemonSprite = data.sprites.front_default;
-    const imgElement    = document.getElementById("pokemonSprite");
+    const imgElement = document.getElementById("pokemonSprite");
 
-    imgElement.src           = pokemonSprite;
+    imgElement.src = pokemonSprite;
     imgElement.style.display = "block";
   } catch (error) {
     console.error(error);
   }
 }
-pokemonName = document.getElementById("pokemonName");
-button = document.getElementBy("submit")
-handeSearch() {
-    value = pokemonName.value
-    console.log(value)
+
+
+// ---- YOUR CODE BELOW ----
+
+// get elements
+let pokemonInput = document.getElementById("pokemonName");
+let button = document.getElementById("searchButton");
+
+button.addEventListener("click", handleSearch);
+
+function handleSearch() {
+
+  let value = pokemonInput.value.trim();
+  let error = document.getElementById("errorMessage");
+  let sprite = document.getElementById("pokemonSprite");
+
+  sprite.style.display = "none";
+  error.textContent = "";
+
+  if (value === "") {
+    error.textContent = "Please enter a Pokémon name.";
+    return;
+  }
+
+  let regex = /^[a-zA-Z-]+$/;
+
+  if (!regex.test(value)) {
+    error.textContent = "Only letters and hyphens are allowed.";
+    return;
+  }
+
+  if (value.length > 30) {
+    error.textContent = "Name must be under 30 characters.";
+    return;
+  }
+  fetchData();
 }
